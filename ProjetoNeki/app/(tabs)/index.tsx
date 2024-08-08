539cbee9378ccc5af34655a5422951d856345625
Leonardo Guerarti Cunha <l.guerarti@gmail.com>
2024-08-08 09:44:02 -0300

    Inicialização do FrontEnd Mobile

---
+++ ProjetoNeki/app/(tabs)/index.tsx
@@ -0,0 +1,15 @@
+import { Image, StyleSheet, Platform } from 'react-native';
+
+import { View,Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
